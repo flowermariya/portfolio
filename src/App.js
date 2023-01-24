5eac74cea86aac7871c02ca211a3fcc588f1ea0a
Flower Maria Varghese <flower.marghese@bolder.no>
2023-01-25 00:50:18 +0530

    contact page - version 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
+import * as React from "react";
 import { AboutPage } from "./components/aboutpage";
 import { Experience } from "./components/experience&education";
 import { Navbar } from "./components/navbar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { Contact } from "./components/contact";
+
 
 const App = () => {
   return (
-    <div class=" bg-black">
+    <div className=" bg-black">
       <Navbar />
-      <AboutPage/>
-      <Experience />
       <Routes>
-        <Route path="/about">{AboutPage}</Route>
-        <Route path="experience" component={Experience} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
--- src/components/aboutpage.jsx
+++ src/components/aboutpage.jsx
@@ -11,109 +11,84 @@ import img from "../assets/img/propic1.jpeg";
 
 export const AboutPage = () => {
   return (
-    <div class="mx-auto my-auto pl-40 pr-40">
-      <div class="container mx-auto my-auto bg-gray-900 flex">
-        <div class="flex flex-wrap">
-          <div class="w-full mb-4 p-4">
+    <div class="mx-auto my-auto pl-40 pr-40 ">
+      <div class="container bg-gray-900 flex">
+        <div class="flex flex-wrap p-4">
+          <div class="w-full p-4">
             <p class=" text-xl font-bold text-3xl text-white">About Me</p>
           </div>
-          <div class="w-1/3 rounded-md">
-            <img
-              src={img}
-              alt="profile image"
-              class="object-cover w-3/4 h-80 px-7"
-            />
-            <div class="mx-auto px-8 py-8 space-x-4 text-white">
-              <button class=" hover:bg-rose-500 inline-flex items-center rounded-full h-12 w-12 justify-center">
-                <BsLinkedin size={20} />
-              </button>
-              <button class=" hover:bg-rose-500 inline-flex items-center rounded-full h-12 w-12 justify-center">
-                <BsTwitter size={20} />
-              </button>
-              <button class=" hover:bg-rose-500 inline-flex items-center rounded-full h-12 w-12 justify-center">
-                <BsFacebook size={20} />
-              </button>
-            </div>
-          </div>
-          <div class="w-1/2 text-xl">
-            <p class="mb-4 font-bold text-white">SOFTWARE DEVELOPER</p>
-            <p class="mb-4 text-justify text-slate-300">
-              I'm Creative Director and UI/UX Designer from Sydney, Australia,
-              working in web development and print media. I enjoy turning
-              complex problems into simple, beautiful and intuitive designs.
-              companies.
-            </p>
-            <p class="mb-4 text-slate-300">
-              My aim is to bring across your message and identity in the most
-              creative way. I created web design for many famous brand
-              companies.
-            </p>
-            <div class="w-full">
-              <p class=" mb-4 text-xl font-bold text-white">Personal Info</p>
-            </div>
-            <div class="row text-white my-4">
-              <div class="col">
-                <div class="flex flex-wrap">
-                  <BsFillTelephoneFill class="text-blue-500 mx-3" size={25} />
-                  <p class="text-slate-300">+919447695491</p>
-                </div>
-              </div>
-              <div class="col">
-                <div class="flex flex-wrap">
-                  <BiMap class="text-green-500 mx-3" size={25} />
-                  <p class="text-slate-300">India, Kerala</p>
-                </div>
+
+          <div class="row p-4">
+            <div class=" w-1/3">
+              <img
+                src={img}
+                alt="profileimage"
+                class="object-cover  w-80 h-80 px-7"
+              />
+              <div class="mx-auto px-8 py-8 space-x-4 text-white">
+                <button class=" hover:bg-rose-500 inline-flex items-center rounded-full h-12 w-12 justify-center">
+                  <BsLinkedin size={20} />
+                </button>
+                <button class=" hover:bg-rose-500 inline-flex items-center rounded-full h-12 w-12 justify-center">
+                  <BsTwitter size={20} />
+                </button>
+                <button class=" hover:bg-rose-500 inline-flex items-center rounded-full h-12 w-12 justify-center">
+                  <BsFacebook size={20} />
+                </button>
               </div>
             </div>
-            <div class="row text-white">
-              <div class="col">
-                <div class="flex flex-wrap">
-                  <AiOutlineMail class=" mx-3 text-red-500" size={25} />
-                  <p class="text-slate-300">flowermariya811</p>
+
+            <div class="w-2/3">
+              <p class="mb-4 font-bold text-xl text-white">SOFTWARE DEVELOPER</p>
+              <p class="mb-4 text-justify text-slate-300">
+                I'm Creative Director and UI/UX Designer from Sydney, Australia,
+                working in web development and print media. I enjoy turning
+                complex problems into simple, beautiful and intuitive designs.
+                companies.
+              </p>
+              <p class="mb-4 text-slate-300">
+                My aim is to bring across your message and identity in the most
+                creative way. I created web design for many famous brand
+                companies.
+              </p>
+              <div class="w-full">
+                <p class=" mb-4 text-xl font-bold text-white">Personal Info</p>
+              </div>
+              <div class="row text-white my-4">
+                <div class="col">
+                  <div class="flex flex-wrap">
+                    <BsFillTelephoneFill class="text-blue-500 mx-3" size={25} />
+                    <p class="text-slate-300">+919447695491</p>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="flex flex-wrap">
+                    <BiMap class="text-green-500 mx-3" size={25} />
+                    <p class="text-slate-300">India, Kerala</p>
+                  </div>
                 </div>
               </div>
-              <div class="col">
-                <div class="flex flex-wrap">
-                  <MdOutlineDateRange class=" mx-3 text-violet-500" size={25} />
-                  <p class="text-slate-300">08th November 1998</p>
+              <div class="row text-white">
+                <div class="col">
+                  <div class="flex flex-wrap">
+                    <AiOutlineMail class=" mx-3 text-red-500" size={25} />
+                    <p class="text-slate-300">flowermariya811@gmail.com</p>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="flex flex-wrap">
+                    <MdOutlineDateRange
+                      class=" mx-3 text-violet-500"
+                      size={25}
+                    />
+                    <p class="text-slate-300">08th November 1998</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="container bg-gray-900 flex">
-        <div class="flex flex-wrap">
-          <div class="w-full p-4">
-            <p class=" text-xl font-bold text-3xl text-white">What I do</p>
-          </div>
-          <div class="row p-4 gap-4">
-            <div class="col p-4 border-2 border-gray-800">
-              <p class=" font-bold text-white mb-2 text-lg">
-                SERVER SIDE MANAGEMENT
-              </p>
-              <p class="text-slate-300 xs:bg-blue-500">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-                diam euismod volutpat.
-              </p>
-            </div>
-            <div class="col p-4 border-2 border-gray-800">
-              <p class=" font-bold text-white mb-2 text-lg">WEB DEVELOPMENT</p>
-              <p class="text-slate-300 xs:bg-blue-500">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-                diam euismod volutpat.
-              </p>
-            </div>
-            <div class="col p-4 border-2 border-gray-800">
-              <p class=" font-bold text-white mb-2 text-lg">
-                SOFTWARE DEVELOPMENT
-              </p>
-              <p class="text-slate-300 xs:bg-blue-500">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-                diam euismod volutpat.
-              </p>
-            </div>
-          </div>
+
+         
         </div>
       </div>
     </div>
